cfba4affe1232e57cda55df5af8fbe41d30e7d0b
Yusuke Sangenya <longinus.eva@gmail.com>
2018-05-13 14:45:55 +0900

    Oshare image.

--- src/text2image.rs
+++ src/text2image.rs
@@ -1,5 +1,6 @@
 use image;
-use imageproc::drawing::draw_text_mut;
+use imageproc::rect::Rect;
+use imageproc::drawing::{draw_text_mut, draw_filled_rect_mut};
 use image::{Rgb, RgbImage};
 use rusttype::{Font, FontCollection, Scale};
 
@@ -7,11 +8,11 @@ use rusttype::{Font, FontCollection, Scale};
 fn text2image_test() {
     use std::path::Path;
     let img = text2image(String::from("私はその人を常に先生と呼んでいた。\nだからここでもただ先生と書くだけで本名はuchiaけない。これは世間を憚かる遠慮というよりも、その方が私にとって自然だからである。\n私はその人の記憶を呼び起すごとに、すぐ「先生」といいたくなる。\n筆を執っても心持は同じ事である。\nよそよそしい頭文字などはとても使う気にならない。"));
-    img.save(Path::new("test.png"));
+    img.save(Path::new("test.png")).unwrap();
 }
 
 pub fn text2image(text: String) -> image::RgbImage {
-    let font = get_font();
+    let font = get_vl_gothic();
 
     let full_font_size = 22.5;
     let half_font_size = full_font_size / 2.;
@@ -20,9 +21,12 @@ pub fn text2image(text: String) -> image::RgbImage {
     let max_text_length = 44;
     let wrapped_text = wrap_text(text, max_text_length);
 
+    let text_height = (wrapped_text.len() as f32) * full_font_size;
     let image_width = 600;
-    let image_height = (50. + (wrapped_text.len() as f32) * full_font_size + 90.) as u32;
-    let mut image = RgbImage::from_pixel(image_width, image_height, Rgb([255,255,255]));
+    let image_height = (50. + text_height + 90.) as u32;
+    let mut image = RgbImage::from_pixel(image_width, image_height, Rgb([0x2c, 0x36, 0x5d]));
+    let rect = Rect::at(20, 20).of_size(560, 30 + (text_height as u32) + 30);
+    draw_filled_rect_mut(&mut image, rect, Rgb([255, 255, 255]));
     let x_offset = 50.;
     let y_offset = 50.;
 
@@ -40,6 +44,16 @@ pub fn text2image(text: String) -> image::RgbImage {
             h_index += char_len(c);
         }
     }
+
+    let logo_scale = Scale { x: 25., y: 25. };
+    let logo_x_position = 30;
+    let logo_y_position = image_height - 45;
+    let tanuki_font = get_tanuki_magic();
+    draw_text_mut(
+        &mut image, Rgb([255, 255, 255]),
+        logo_x_position, logo_y_position,
+        logo_scale, &tanuki_font, "Reing"
+    );
 
     return image;
 }
@@ -90,9 +104,14 @@ fn wrap_text(text: String, max_length: u32 /* in ascii character */) -> Vec<Stri
     return wrapped_lines;
 }
 
-fn get_font<'a>() -> Font<'a> {
+fn get_vl_gothic<'a>() -> Font<'a> {
     let font = Vec::from(include_bytes!("../assets/font/VL-Gothic-Regular.ttf") as &[u8]);
     FontCollection::from_bytes(font).unwrap().into_font().unwrap()
+}
+
+fn get_tanuki_magic<'a>() -> Font<'a> {
+    let font = Vec::from(include_bytes!("../assets/font/TanukiMagic.ttf") as &[u8]);
+    FontCollection::from_bytes(font).unwrap().into_font().unwrap()    
 }
 
 fn char_len(c: char) -> u32 { // in ascii character
