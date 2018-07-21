47542de24c80dc9b6a45d797c16f57f496787afa
Yusuke Sangenya <longinus.eva@gmail.com>
2018-07-21 11:31:47 +0900

    Pull from VPS.

--- scripts/pull_db.sh
+++ scripts/pull_db.sh
@@ -1,6 +1,7 @@
+PRODUCTION_DATABASE_URL=postgresql://postgres:postgres@127.0.0.1:5432/reing
 LOCAL_DATABASE_URL=postgres://reing:reing@127.0.0.1:5432/reing
 psql $LOCAL_DATABASE_URL -c "
 drop schema public cascade;
 create schema public;
 "
-PGSSLMODE=disable heroku pg:pull DATABASE_URL $LOCAL_DATABASE_URL --remote heroku
+ssh kuminecraft.xyz pg_dump -Ft $PRODUCTION_DATABASE_URL | pg_restore -d $LOCAL_DATABASE_URL
