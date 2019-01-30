6a90b1b5eeafd091dce4602aefdeedf0bcfdabf2
EC2 Default User <ec2-user@ip-172-31-7-215.ap-northeast-1.compute.internal>
2019-01-30 13:08:28 +0000

    bench

--- benchrunner.sh
+++ benchrunner.sh
@@ -1,9 +1,8 @@
 set -e
-echo "concurrency,workers,longest_request(ms)" > $1
 
-for C in $(seq 1 12)
+for C in $(seq 1 100)
 do
-    for W in $(seq 1 12)
+    for W in $(seq 1 100)
     do
         echo "concurrency: $C, worker: $W"
         cat Rocket.toml.template | sed -e "s/worker = 1/worker = ${W}/g" > Rocket.toml
@@ -11,8 +10,7 @@ do
         ROCKET_PID=$!
         sleep 1
         ab -n 1000 -c $C http://localhost:8000/ # warm up
-        LONGEST_REQUEST=$(ab -n 2000 -c $C http://localhost:8000/ 2> /dev/null | grep "longest request" | awk '{ print $2 }')
-        echo "$C,$W,$LONGEST_REQUEST" >> $1
+        ab -n 2000 -c $C -e "benchresults/c${C}_w${W}.csv" http://localhost:8000/
         kill $ROCKET_PID
         sleep 1
     done
--- scripts/pull_db.sh
+++ scripts/pull_db.sh
@@ -1,7 +1,7 @@
 PRODUCTION_DATABASE_URL=postgresql://postgres:postgres@127.0.0.1:5432/reing
 LOCAL_DATABASE_URL=postgres://reing:reing@127.0.0.1:5432/reing
-psql $LOCAL_DATABASE_URL -c "
+/usr/pgsql-10/bin/psql $LOCAL_DATABASE_URL -c "
 drop schema public cascade;
 create schema public;
 "
-ssh kuminecraft.xyz pg_dump -Ft $PRODUCTION_DATABASE_URL | pg_restore -d $LOCAL_DATABASE_URL
+ssh sangenya@kuminecraft.xyz pg_dump -Ft $PRODUCTION_DATABASE_URL | /usr/pgsql-10/bin/pg_restore -d $LOCAL_DATABASE_URL
