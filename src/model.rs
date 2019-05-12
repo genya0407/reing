e726f477ba6e638ee2605bba7741ddca977dbead
Yusuke Sangenya <longinus.eva@gmail.com>
2019-05-12 16:19:11 +0900

    modify model

--- src/model.rs
+++ src/model.rs
@@ -4,7 +4,6 @@ use r2d2_diesel;
 use diesel;
 use db;
 use std::ops::Deref;
-use std::collections::HashMap;
 use diesel::RunQueryDsl;
 use diesel::QueryDsl;
 use diesel::SaveChangesDsl;
@@ -19,21 +18,15 @@ pub struct Question {
     pub body: String,
     pub ip_address: String,
     pub created_at: DateTime<Local>,
-    pub hidden: bool,
-    pub answers: Vec<Answer>,
-}
-
-impl Question {
-    pub fn answered(&self) -> bool {
-        return !self.answers.is_empty()
-    }
+    pub hidden: bool
 }
 
 #[derive(Debug, Clone)]
 pub struct Answer {
     pub id: i32,
     pub body: String,
-    pub created_at: DateTime<Local>
+    pub created_at: DateTime<Local>,
+    pub question: Question
 }
 
 pub struct Repository {
@@ -63,11 +56,11 @@ impl Repository {
                     .values(&new_question)
                     .get_result(self.conn())
                     .expect("Error saving new question");
-            Ok(self.qas2questions(vec![(q, None)]).into_iter().next().unwrap())
+            Ok(self.db2model_question(q))
         }
     }
 
-    pub fn store_answer(&self, id: i32, body: String) -> Option<Question> {
+    pub fn store_answer(&self, id: i32, body: String) -> Option<Answer> {
         self.find_question(id).map(|mut question| {
             let new_answer = db::NewAnswer {
                 question_id: question.id,
@@ -77,83 +70,83 @@ impl Repository {
                 .values(&new_answer)
                 .get_result(self.conn())
                 .expect("Error saving new answer");
-            let answer = self.row2answer(a);
-            question.answers.push(answer);
-            question
+            
+            self.db2model_answer(a, question)
         })
     }
 
-    pub fn answered_questions(&self, offset: i64, count: i64) -> Vec<Question> {
-        let qas = questions::table
-                .left_join(answers::table)
-                .filter(answers::id.is_not_null())
+    pub fn answers(&self, offset: i64, count: i64) -> Vec<Question> {
+        let answers = answers::table
+                .inner_join(questions::table)
+                .order(answers::created_at.desc())
                 .offset(offset)
                 .limit(count)
-                .order(answers::created_at.desc())
-                .load::<(db::Question, Option<db::Answer>)>(self.conn())
+                .load::<(db::Answer, db::Question)>(self.conn())
                 .unwrap();
-        self.qas2questions(qas)
+        answers.into_iter().map(|(a, q)| self.db2model_answer(a, self.db2model_question(q))).collect()
     }
 
     pub fn not_answered_questions(&self) -> Vec<Question> {
-        let qas = questions::table
+        let qs = questions::table
                 .left_join(answers::table)
                 .filter(answers::id.is_null())
                 .order(answers::created_at.desc())
                 .load::<(db::Question, Option<db::Answer>)>(self.conn())
                 .unwrap();
-        self.qas2questions(qas)                
+        qs.into_iter().map(|(q, _)| self.db2model_question(q)).collect()
     }
 
-    pub fn search_questions(&self, query: String) -> Vec<Question> {
+    pub fn search_answers(&self, query: String) -> Vec<Answer> {
         use diesel::TextExpressionMethods;
         use diesel::BoolExpressionMethods;
 
-        let qas = questions::table
-                .left_join(answers::table)
-                .filter(answers::id.is_not_null())
+        let answers = answers::table
+                .inner_join(questions::table)
                 .filter(questions::body.like(&format!("%{}%", query)).or(answers::body.like(&format!("%{}%", query))))
                 .order(answers::created_at.desc())
-                .load::<(db::Question, Option<db::Answer>)>(self.conn())
+                .load::<(db::Answer, db::Question)>(self.conn())
                 .unwrap();
-        self.qas2questions(qas)
+        answers.into_iter().map(|(a, q)| self.db2model_answer(a, self.db2model_question(q))).collect()
     }
 
-    pub fn find_question(&self, id: i32) -> Option<Question> {
-        let qas = questions::table
-                .left_join(answers::table)
+    pub fn find_answer(&self, id: i32) -> Option<Answer> {
+        let answer = answers::table
+                .inner_join(questions::table)
                 .filter(questions::id.eq(id))
                 .limit(1)
-                .load::<(db::Question, Option<db::Answer>)>(self.conn())
-                .unwrap();
-        self.qas2questions(qas).into_iter().next()
+                .load::<(db::Answer, db::Question)>(self.conn())
+                .unwrap()
+                .first()
+                .cloned();
+        answer.map(|(a, q)| self.db2model_answer(a, self.db2model_question(q)))
     }
 
-    pub fn find_next_question(&self, id: i32) -> Option<Question> {
-        let qas = questions::table
-                .left_join(answers::table)
-                .filter(questions::id.gt(id))
-                .filter(answers::id.is_not_null())
-                .order(questions::id.asc())
+    pub fn find_next_answer(&self, after: DateTime<Local>) -> Option<Answer> {
+        let answer = answers::table
+                .inner_join(questions::table)
+                .filter(answers::created_at.gt(after))
+                .order(answers::created_at.asc())
                 .limit(1)
-                .load::<(db::Question, Option<db::Answer>)>(self.conn())
-                .unwrap();
-        self.qas2questions(qas).into_iter().next()
+                .load::<(db::Answer, db::Question)>(self.conn())
+                .unwrap()
+                .first()
+                .cloned();
+        answer.map(|(a, q)| self.db2model_answer(a, self.db2model_question(q)))
     }
 
-    pub fn find_prev_question(&self, id: i32) -> Option<Question> {
-        let qas = questions::table
-                .left_join(answers::table)
-                .filter(questions::id.lt(id))
-                .filter(answers::id.is_not_null())
-                .order(questions::id.desc())
+    pub fn find_prev_answer(&self, before: DateTime<Local>) -> Option<Answer> {
+        let answer = answers::table
+                .inner_join(questions::table)
+                .filter(answers::created_at.lt(before))
+                .order(answers::created_at.desc())
                 .limit(1)
-                .load::<(db::Question, Option<db::Answer>)>(self.conn())
-                .unwrap();
-        self.qas2questions(qas).into_iter().next()
+                .load::<(db::Answer, db::Question)>(self.conn())
+                .unwrap()
+                .first()
+                .cloned();
+        answer.map(|(a, q)| self.db2model_answer(a, self.db2model_question(q)))
     }
 
-    // this method doesn't update answers.
     pub fn update_question(&self, question: Question) {
         let q = db::QuestionForm {
             id: question.id,
@@ -165,44 +158,22 @@ impl Repository {
         q.save_changes::<db::Question>(self.conn()).unwrap();
     }
 
-    fn qas2questions(&self, qas: Vec<(db::Question, Option<db::Answer>)>) -> Vec<Question> {
-        let mut q_map: HashMap<i32, Question> = HashMap::new();
-        for (q, a_opt) in qas {
-            q_map.entry(q.id)
-                .and_modify(|question| {
-                    if let Some(a) = a_opt.clone() {
-                        question.answers.push(self.row2answer(a))
-                    }
-                }).or_insert_with(|| {
-                    let mut question = self.row2question(q);
-                    if let Some(a) = a_opt {
-                        question.answers.push(self.row2answer(a))
-                    }
-                    question
-                });
-        }
-
-        let mut result = q_map.into_iter().map(|(_,v)| v).collect::<Vec<Question>>();
-        result.sort_by_key(|q| q.created_at);
-        return result;
-    }
-
-    fn row2question(&self, q: db::Question) -> Question {
+    fn db2model_question(&self, q: db::Question) -> Question {
         Question {
             id: q.id,
             body: q.body,
             ip_address: q.ip_address,
             created_at: q.created_at.with_timezone(&Local),
-            hidden: q.hidden,
-            answers: vec![]
+            hidden: q.hidden
         }
     }
 
-    fn row2answer(&self, a: db::Answer) -> Answer {
+    fn db2model_answer(&self, a: db::Answer, q: Question) -> Answer {
         Answer {
             id: a.id,
             body: a.body,
-            created_at: a.created_at.with_timezone(&Local)
+            created_at: a.created_at.with_timezone(&Local),
+            question: q
         }
     }
 }
