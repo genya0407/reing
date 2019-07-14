2f0a42802188ef1672c7b2fb40026ad68032b0c7
Yusuke Sangenya <longinus.eva@gmail.com>
2019-07-15 08:34:35 +0900

    tmp

--- src/usecase/viewer.rs
+++ src/usecase/viewer.rs
@@ -21,71 +21,73 @@ pub trait SeeAllAnswers: UsesAnswerRepository {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+  mod see_all_answers {
+    use super::super::*;
 
-  #[derive(Clone)]
-  struct MockAnswerRepository {
-    answers: Vec<Answer>
-  }
-  impl AnswerRepository for MockAnswerRepository {
-    fn find_all(&self) -> Vec<Answer> {
-      self.answers.clone()
+    #[derive(Clone)]
+    struct MockAnswerRepository {
+      answers: Vec<Answer>
     }
-
-    fn store(&self, answer: Answer) {
-      panic!("Not implemented")
-    }
-
-    fn find(&self, id: Uuid) -> Option<Answer> {
-      panic!("Not implemented")
-    }
-  }
-
-  struct SeeAllAnswersWithMock {
-    answer_repository_mock: MockAnswerRepository
-  }
-  impl UsesAnswerRepository for SeeAllAnswersWithMock {
-    fn answer_repository(&self) -> Box<AnswerRepository> {
-      Box::new(self.answer_repository_mock.clone())
-    }
-  }
-  impl SeeAllAnswers for SeeAllAnswersWithMock {}
-
-  struct MockSeeAllAnswersOutputPort {
-    expected_results: Vec<Answer>
-  }
-  impl SeeAllAnswersOutputPort for MockSeeAllAnswersOutputPort {
-    fn output(&self, answers: Vec<Answer>) {
-      assert_eq!(self.expected_results, answers)
-    }
-  }
-
-  #[test]
-  fn test_SeeAllAnswersImpl() {
-    let question = Question {
-      id: Uuid::new_v4(),
-      body: String::from("aaa"),
-      ip_address: String::from("0.0.0.0"),
-      hidden: false,
-      created_at: Local::now(),
-    };
-    let answers = vec![
-      Answer {
-        id: Uuid::new_v4(),
-        body: "answer1".to_string(),
-        created_at: Local::now(),
-        question: question.clone(),
-      },
-      Answer{
-        id: Uuid::new_v4(),
-        body: "answer2".to_string(),
-        created_at: Local::now(),
-        question: question.clone(),
+    impl AnswerRepository for MockAnswerRepository {
+      fn find_all(&self) -> Vec<Answer> {
+        self.answers.clone()
       }
-    ];
-    let mar = MockAnswerRepository{ answers: answers.clone() };
-    let sawm = SeeAllAnswersWithMock { answer_repository_mock: mar };
-    let mo = MockSeeAllAnswersOutputPort { expected_results: answers.clone() };
-    sawm.execute(Box::new(mo));
+
+      fn store(&self, answer: Answer) {
+        panic!("Not implemented")
+      }
+
+      fn find(&self, id: Uuid) -> Option<Answer> {
+        panic!("Not implemented")
+      }
+    }
+
+    struct SeeAllAnswersWithMock {
+      answer_repository_mock: MockAnswerRepository
+    }
+    impl UsesAnswerRepository for SeeAllAnswersWithMock {
+      fn answer_repository(&self) -> Box<AnswerRepository> {
+        Box::new(self.answer_repository_mock.clone())
+      }
+    }
+    impl SeeAllAnswers for SeeAllAnswersWithMock {}
+
+    struct MockSeeAllAnswersOutputPort {
+      expected_results: Vec<Answer>
+    }
+    impl SeeAllAnswersOutputPort for MockSeeAllAnswersOutputPort {
+      fn output(&self, answers: Vec<Answer>) {
+        assert_eq!(self.expected_results, answers)
+      }
+    }
+
+    #[test]
+    fn test_SeeAllAnswersImpl() {
+      let question = Question {
+        id: Uuid::new_v4(),
+        body: String::from("aaa"),
+        ip_address: String::from("0.0.0.0"),
+        hidden: false,
+        created_at: Local::now(),
+      };
+      let answers = vec![
+        Answer {
+          id: Uuid::new_v4(),
+          body: "answer1".to_string(),
+          created_at: Local::now(),
+          question: question.clone(),
+        },
+        Answer{
+          id: Uuid::new_v4(),
+          body: "answer2".to_string(),
+          created_at: Local::now(),
+          question: question.clone(),
+        }
+      ];
+      let mar = MockAnswerRepository{ answers: answers.clone() };
+      let sawm = SeeAllAnswersWithMock { answer_repository_mock: mar };
+      let mo = MockSeeAllAnswersOutputPort { expected_results: answers.clone() };
+      sawm.execute(Box::new(mo));
+    }
   }
 }
