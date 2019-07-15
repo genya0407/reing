60f62add4f6c0bee0d5473167730ed83896ed6e4
Yusuke Sangenya <longinus.eva@gmail.com>
2019-07-15 11:50:26 +0900

    tmp

--- src/usecase/viewer.rs
+++ src/usecase/viewer.rs
@@ -1,7 +1,4 @@
-use super::repository::{
-  AnswerRepository,
-  QuestionRepository
-};
+use super::repository::AnswerRepository;
 use crate::entity::{Answer, Question};
 use uuid::Uuid;
 use chrono::Local;
@@ -63,10 +60,16 @@ pub mod see_all_answers {
         *data = answers;
       }
     }
+  }
+
+  #[cfg(test)]
+  mod tests {
+    use super::*;
+    use super::mock::*;
 
     #[test]
     fn test_mock_output_port() {
-      let mop = MockOutputPort::new();
+      let mop = mock::MockOutputPort::new();
       let question = Question {
         id: Uuid::new_v4(),
         body: String::from("aaa"),
@@ -94,11 +97,6 @@ pub mod see_all_answers {
       let output_answers = mop.result.lock().unwrap();
       assert_eq!(output_answers.len(), 2)
     }
-  }
-
-  #[cfg(test)]
-  mod tests {
-    use super::*;
 
     #[test]
     fn test_usecase() {
@@ -117,7 +115,7 @@ pub mod see_all_answers {
           }
         }
       );
-      let oport = mock::MockOutputPort::new();
+      let oport = MockOutputPort::new();
       let usecase = super::new(repo);
       usecase.execute(oport.clone());
 
@@ -187,6 +185,29 @@ pub mod see_answer_detail {
       }
     }
 
+    #[derive(Clone)]
+    pub struct MockInputPort {
+      pub data: Uuid
+    }
+
+    impl MockInputPort {
+      pub fn new(id: Uuid) -> Box<MockInputPort> {
+        Box::new(Self { data: id })
+      }
+    }
+
+    impl InputPort for MockInputPort {
+      fn input(&self) -> Uuid {
+        self.data.clone()
+      }
+    }
+  }
+
+  #[cfg(test)]
+  mod tests {
+    use super::*;
+    use super::mock::*;
+
     #[test]
     fn test_mock_output_port() {
       let mop = MockOutputPort::new();
@@ -207,28 +228,36 @@ pub mod see_answer_detail {
       assert_eq!(*mop.result.lock().unwrap(), Some(answer));
     }
 
-    #[derive(Clone)]
-    pub struct MockInputPort {
-      pub data: Uuid
-    }
-
-    impl MockInputPort {
-      pub fn new(id: Uuid) -> Box<MockInputPort> {
-        Box::new(Self { data: id })
-      }
-    }
-
-    impl InputPort for MockInputPort {
-      fn input(&self) -> Uuid {
-        self.data.clone()
-      }
-    }
 
     #[test]
     fn test_mock_input_port() {
       let id = Uuid::new_v4();
       let mip = MockInputPort::new(id);
       assert_eq!(mip.input(), id)
+    }
+
+    #[test]
+    fn test_usecase() {
+      let answer = Answer {
+        id: Uuid::new_v4(),
+        body: "answer1".to_string(),
+        created_at: Local::now(),
+        question: Question {
+          id: Uuid::new_v4(),
+          body: String::from("aaa"),
+          ip_address: String::from("0.0.0.0"),
+          hidden: false,
+          created_at: Local::now(),
+        }
+      };
+      let repo = MockAnswerRepository::new();
+      repo.store(answer.clone());
+      let usecase = new(repo);
+      let iport = mock::MockInputPort::new(answer.id.clone());
+      let oport = mock::MockOutputPort::new();
+      usecase.execute(iport, oport.clone());
+
+      assert_eq!(*oport.result.lock().unwrap(), Some(answer));
     }
   }
 }
