deb0d3bcb5240a396c837f6c52d2e71bcf53ae05
Yusuke Sangenya <longinus.eva@gmail.com>
2019-07-27 09:55:00 +0900

    implement

--- src/usecase/answerer/see_all_not_answered_questions.rs
+++ src/usecase/answerer/see_all_not_answered_questions.rs
@@ -5,7 +5,7 @@ use crate::usecase::{InputPort, OutputPort};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct AnswererAuthenticationDTO {
-  pub answerer_id: String,
+  pub answerer_id: Uuid,
   pub password: String
 }
 
@@ -25,17 +25,37 @@ pub trait Usecase {
   fn execute(&self, iport: Box<InputPort<AnswererAuthenticationDTO>>, oport: Box<OutputPort<Result<Vec<QuestionDTO>, SeeAllNotAnsweredQuestionsError>>>);
 }
 
-// mod implementation {
-//   use crate::usecase::{InputPort, OutputPort};
+mod implementation {
+  use crate::usecase::{InputPort, OutputPort};
+  use crate::usecase::repository::*;
+  use super::*;
 
-//   struct Usecase {
-//     repo: Box<QuestionRepository>
-//   }
+  struct Usecase {
+    question_repo: Box<QuestionRepository>,
+    answerer_repo: Box<AnswererRepository>,
+  }
 
-//   impl super::Usecase for Usecase {
-//     fn execute(&self, iport: Box<InputPort<AnswererAuthDTO>>, oport: Box<OutputPort<Result<Vec<QuestionDTO>, SeeAllNotAnsweredQuestionError>>>) {
-//       let auth = iport.input();
-//       self.repo.find
-//     }
-//   }
-// }
+  impl super::Usecase for Usecase {
+    fn execute(&self, iport: Box<InputPort<AnswererAuthenticationDTO>>, oport: Box<OutputPort<Result<Vec<QuestionDTO>, SeeAllNotAnsweredQuestionsError>>>) {
+      let auth = iport.input();
+      let answerer_opt = self.answerer_repo.find(auth.answerer_id);
+      let output = match answerer_opt.map(|a| a.authenticate(auth.password.clone())) {
+        Some(true) => Ok(
+          self.question_repo
+              .find_all_not_answered_yet_of(auth.answerer_id)
+              .into_iter()
+              .map(|q|
+                QuestionDTO {
+                  question_id: q.id,
+                  question_body: q.body,
+                  questioned_at: q.created_at
+                }
+              )
+              .collect()
+        ),
+        _ => Err(SeeAllNotAnsweredQuestionsError::AuthenticationFailed),
+      };
+      oport.output(output);
+    }
+  }
+}
