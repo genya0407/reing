c710c2380201cb3a966eb1621e695239f7300595
Yusuke Sangenya <longinus.eva@gmail.com>
2019-11-03 09:49:26 +0900

    RLS

--- src/model.rs
+++ src/model.rs
@@ -1,16 +1,17 @@
 use chrono::prelude::*;
+use db;
+use db::schema::{answers, questions};
+use diesel;
+use diesel::ExpressionMethods;
+use diesel::QueryDsl;
+use diesel::RunQueryDsl;
+use diesel::SaveChangesDsl;
 use r2d2;
 use r2d2_diesel;
-use diesel;
-use db;
 use std::ops::Deref;
-use diesel::RunQueryDsl;
-use diesel::QueryDsl;
-use diesel::SaveChangesDsl;
-use diesel::ExpressionMethods;
-use db::schema::{questions, answers};
 
-type DieselConnection = r2d2::PooledConnection<r2d2_diesel::ConnectionManager<diesel::PgConnection>>;
+type DieselConnection =
+    r2d2::PooledConnection<r2d2_diesel::ConnectionManager<diesel::PgConnection>>;
 
 #[derive(Debug, Clone)]
 pub struct Question {
@@ -18,7 +19,7 @@ pub struct Question {
     pub body: String,
     pub ip_address: String,
     pub created_at: DateTime<Local>,
-    pub hidden: bool
+    pub hidden: bool,
 }
 
 #[derive(Debug, Clone)]
@@ -26,87 +27,108 @@ pub struct Answer {
     pub id: i32,
     pub body: String,
     pub created_at: DateTime<Local>,
-    pub question: Question
+    pub question: Question,
 }
 
 pub struct Repository {
-    pooled_connection: DieselConnection
+    pooled_connection: DieselConnection,
 }
 
 pub enum StoreQuestionError {
-    BlankBody
+    BlankBody,
 }
 
 impl Repository {
     pub fn new(pooled_connection: DieselConnection) -> Self {
-        Self { pooled_connection: pooled_connection }
+        Self {
+            pooled_connection: pooled_connection,
+        }
     }
 
     fn conn(&self) -> &diesel::PgConnection {
         self.pooled_connection.deref()
     }
 
-    pub fn store_question(&self, body: String, ip_address: String) -> Result<Question, StoreQuestionError> {
+    pub fn store_question(
+        &self,
+        body: String,
+        ip_address: String,
+    ) -> Result<Question, StoreQuestionError> {
         if body.chars().all(|c| char::is_whitespace(c)) {
             Err(StoreQuestionError::BlankBody)
         } else {
-            let new_question = db::NewQuestion { body: body, ip_address: ip_address };
+            let new_question = db::NewQuestion {
+                body: body,
+                ip_address: ip_address,
+            };
 
             let q: db::Question = diesel::insert_into(questions::table)
-                    .values(&new_question)
-                    .get_result(self.conn())
-                    .expect("Error saving new question");
+                .values(&new_question)
+                .get_result(self.conn())
+                .expect("Error saving new question");
             Ok(self.db2model_question(q))
         }
     }
 
     pub fn store_answer(&self, question_id: i32, body: String) -> Option<Answer> {
-        self.find_question(question_id).map(|question| { // question_idのquestionが存在することを確認
+        self.find_question(question_id).map(|question| {
+            // question_idのquestionが存在することを確認
             let new_answer = db::NewAnswer {
                 question_id: question.id,
-                body: body
+                body: body,
             };
             let a: db::Answer = diesel::insert_into(answers::table)
                 .values(&new_answer)
                 .get_result(self.conn())
                 .expect("Error saving new answer");
-            
             self.db2model_answer(a, question)
         })
     }
 
     pub fn answers(&self, offset: i64, count: i64) -> Vec<Answer> {
         let answers = answers::table
-                .inner_join(questions::table)
-                .order(answers::created_at.desc())
-                .offset(offset)
-                .limit(count)
-                .load::<(db::Answer, db::Question)>(self.conn())
-                .unwrap();
-        answers.into_iter().map(|(a, q)| self.db2model_answer(a, self.db2model_question(q))).collect()
+            .inner_join(questions::table)
+            .order(answers::created_at.desc())
+            .offset(offset)
+            .limit(count)
+            .load::<(db::Answer, db::Question)>(self.conn())
+            .unwrap();
+        answers
+            .into_iter()
+            .map(|(a, q)| self.db2model_answer(a, self.db2model_question(q)))
+            .collect()
     }
 
     pub fn not_answered_questions(&self) -> Vec<Question> {
         let qs = questions::table
-                .left_join(answers::table)
-                .filter(answers::id.is_null())
-                .order(answers::created_at.desc())
-                .load::<(db::Question, Option<db::Answer>)>(self.conn())
-                .unwrap();
-        qs.into_iter().map(|(q, _)| self.db2model_question(q)).collect()
+            .left_join(answers::table)
+            .filter(answers::id.is_null())
+            .order(answers::created_at.desc())
+            .load::<(db::Question, Option<db::Answer>)>(self.conn())
+            .unwrap();
+        qs.into_iter()
+            .map(|(q, _)| self.db2model_question(q))
+            .collect()
     }
 
     pub fn search_answers(&self, query: String) -> Vec<Answer> {
-        use diesel::TextExpressionMethods;
         use diesel::BoolExpressionMethods;
+        use diesel::TextExpressionMethods;
 
         let answers = answers::table
-                .inner_join(questions::table)
-                .filter(questions::body.like(&format!("%{}%", query)).or(answers::body.like(&format!("%{}%", query))))
-                .order(answers::created_at.desc())
-                .load::<(db::Answer, db::Question)>(self.conn())
-                .unwrap();
-        answers.into_iter().map(|(a, q)| self.db2model_answer(a, self.db2model_question(q))).collect()
+            .inner_join(questions::table)
+            .filter(
+                questions::body
+                    .like(&format!("%{}%", query))
+                    .or(answers::body.like(&format!("%{}%", query))),
+            )
+            .order(answers::created_at.desc())
+            .load::<(db::Answer, db::Question)>(self.conn())
+            .unwrap();
+        answers
+            .into_iter()
+            .map(|(a, q)| self.db2model_answer(a, self.db2model_question(q)))
+            .collect()
     }
 
     pub fn find_question(&self, question_id: i32) -> Option<Question> {
@@ -122,51 +144,51 @@ impl Repository {
 
     pub fn find_answer(&self, answer_id: i32) -> Option<Answer> {
         let answer = answers::table
-                .inner_join(questions::table)
-                .filter(answers::id.eq(answer_id))
-                .limit(1)
-                .load::<(db::Answer, db::Question)>(self.conn())
-                .unwrap()
-                .first()
-                .cloned();
+            .inner_join(questions::table)
+            .filter(answers::id.eq(answer_id))
+            .limit(1)
+            .load::<(db::Answer, db::Question)>(self.conn())
+            .unwrap()
+            .first()
+            .cloned();
         answer.map(|(a, q)| self.db2model_answer(a, self.db2model_question(q)))
     }
 
     pub fn find_answer_by_question_id(&self, question_id: i32) -> Option<Answer> {
         let answer = answers::table
-                .inner_join(questions::table)
-                .filter(questions::id.eq(question_id))
-                .limit(1)
-                .load::<(db::Answer, db::Question)>(self.conn())
-                .unwrap()
-                .first()
-                .cloned();
+            .inner_join(questions::table)
+            .filter(questions::id.eq(question_id))
+            .limit(1)
+            .load::<(db::Answer, db::Question)>(self.conn())
+            .unwrap()
+            .first()
+            .cloned();
         answer.map(|(a, q)| self.db2model_answer(a, self.db2model_question(q)))
     }
 
     pub fn find_next_answer(&self, after: DateTime<Local>) -> Option<Answer> {
         let answer = answers::table
-                .inner_join(questions::table)
-                .filter(answers::created_at.gt(after))
-                .order(answers::created_at.asc())
-                .limit(1)
-                .load::<(db::Answer, db::Question)>(self.conn())
-                .unwrap()
-                .first()
-                .cloned();
+            .inner_join(questions::table)
+            .filter(answers::created_at.gt(after))
+            .order(answers::created_at.asc())
+            .limit(1)
+            .load::<(db::Answer, db::Question)>(self.conn())
+            .unwrap()
+            .first()
+            .cloned();
         answer.map(|(a, q)| self.db2model_answer(a, self.db2model_question(q)))
     }
 
     pub fn find_prev_answer(&self, before: DateTime<Local>) -> Option<Answer> {
         let answer = answers::table
-                .inner_join(questions::table)
-                .filter(answers::created_at.lt(before))
-                .order(answers::created_at.desc())
-                .limit(1)
-                .load::<(db::Answer, db::Question)>(self.conn())
-                .unwrap()
-                .first()
-                .cloned();
+            .inner_join(questions::table)
+            .filter(answers::created_at.lt(before))
+            .order(answers::created_at.desc())
+            .limit(1)
+            .load::<(db::Answer, db::Question)>(self.conn())
+            .unwrap()
+            .first()
+            .cloned();
         answer.map(|(a, q)| self.db2model_answer(a, self.db2model_question(q)))
     }
 
@@ -176,7 +198,7 @@ impl Repository {
             body: question.body,
             ip_address: question.ip_address,
             hidden: question.hidden,
-            created_at: question.created_at.with_timezone(&Utc)
+            created_at: question.created_at.with_timezone(&Utc),
         };
         q.save_changes::<db::Question>(self.conn()).unwrap();
     }
@@ -187,7 +209,7 @@ impl Repository {
             body: q.body,
             ip_address: q.ip_address,
             created_at: q.created_at.with_timezone(&Local),
-            hidden: q.hidden
+            hidden: q.hidden,
         }
     }
 
@@ -196,7 +218,7 @@ impl Repository {
             id: a.id,
             body: a.body,
             created_at: a.created_at.with_timezone(&Local),
-            question: q
+            question: q,
         }
     }
 }
