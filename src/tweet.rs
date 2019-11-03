95928ae48562feb0d4496daa26da094618f3de3f
Yusuke Sangenya <longinus.eva@gmail.com>
2019-11-03 13:33:38 +0900

    Dont upload image

--- src/tweet.rs
+++ src/tweet.rs
@@ -1,14 +1,9 @@
 use super::model;
 use egg_mode;
-use egg_mode::media::{media_types, UploadBuilder};
 use egg_mode::tweet::DraftTweet;
 use std::env;
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
 use tokio::prelude::Future;
 use tokio::runtime::current_thread::block_on_all;
-use uuid::Uuid;
 
 fn get_twitter_access_token() -> Result<egg_mode::Token, std::env::VarError> {
     let con_token = egg_mode::KeyPair::new(
@@ -42,22 +37,6 @@ pub fn get_twitter_username() -> String {
 }
 
 pub fn tweet_answer(answer: model::Answer) {
-    let question_image = reing_text2image::TextImage::new(
-        answer.question.body,
-        String::from("Reing"),
-        (0x2c, 0x36, 0x5d),
-    );
-    let tmp_filepath = format!("/tmp/{}.jpg", Uuid::new_v4());
-    let tmp_filepath = Path::new(&tmp_filepath);
-    question_image
-        .save_image(&tmp_filepath)
-        .expect("failed to save image");
-    let mut image_buf = vec![];
-    File::open(&tmp_filepath)
-        .expect("failed to open image file")
-        .read_to_end(&mut image_buf)
-        .expect("failed to read image file");
-
     let question_url = format!(
         "https://{}/answer/{}",
         env::var("APPLICATION_DOMAIN").expect("failed to fetch environment variable"),
@@ -69,8 +48,6 @@ pub fn tweet_answer(answer: model::Answer) {
         Ok(token) => token,
         _ => return,
     };
-    let builder = UploadBuilder::new(image_buf, media_types::image_jpg());
-    let media_handle = block_on_all(builder.call(&token)).expect("failed to upload media");
-    let draft = DraftTweet::new(tweet_text).media_ids(&[media_handle.id]);
+    let draft = DraftTweet::new(tweet_text);
     block_on_all(draft.send(&token)).expect("failed to send tweet");
 }
